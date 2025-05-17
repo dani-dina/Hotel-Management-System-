b74a885c34812217a10a42c2e851b2a4b164c35d
Daniel <danielkibret8@gmail.com>
2025-05-17 23:27:47 +0300

    controller

--- backend/src/controller/foodMenu.controller.js
+++ backend/src/controller/foodMenu.controller.js
@@ -4,7 +4,7 @@ import { HTTP_STATUS } from "../constants/index.js";
 const findFood = async(name)=>{
     return await Food.findOne({name});
 }
-/* Get All Foods */
+
 const getAllFoods = async(req,res)=>{
     try{
         const getFood = await Food.find();
@@ -13,7 +13,7 @@ const getAllFoods = async(req,res)=>{
         return res.status(HTTP_STATUS.SERVER_ERROR).json("Internal Server Error !");
     }
 }
-// Get Food by id
+
 const getFoodById = async(req,res)=>{
     try{
         const food = await Food.findById(req.params.id);
@@ -25,7 +25,7 @@ const getFoodById = async(req,res)=>{
         return res.status(HTTP_STATUS.SERVER_ERROR).json({ message : "Internal Server Error"});
     }
 }
-/* Add a new Employee */
+
 const addNewFood = async(req,res)=>{
     try{
         const  {
@@ -58,7 +58,7 @@ const addNewFood = async(req,res)=>{
         res.status(HTTP_STATUS.SERVER_ERROR).json({ message : "Internal Server Error !"});
     }
 }
-/* update Food by id */
+
 const updateFoodById = async(req,res)=>{
     try{
         const updateFood = await Food.findByIdAndUpdate(req,params.id,req.body,{ new : true});
