77931999440b45c0d3af158d723f9ea6b74e9e15
Daniel <danielkibret8@gmail.com>
2025-05-23 23:46:22 +0300

    UI

--- frontend/src/pages/404.jsx
+++ frontend/src/pages/404.jsx
@@ -2,6 +2,7 @@ const NotFound = () => {
   return (
     <div className="w-full h-full flex items-center justify-center">
       <h2 className='text-3xl font-bold'>OOPs Page Not Found</h2>
+      <br />
       <h1 className=''>404</h1>
     </div>
   );
