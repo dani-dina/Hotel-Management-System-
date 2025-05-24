1fa8ae7cd332dfdc7117f0c300a7a24239ee5e56
Daniel <danielkibret8@gmail.com>
2025-05-24 22:39:05 +0300

    UI

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -3,10 +3,12 @@ import banner from '../assets/banner.jpg';
 const Hero = () => {
   return (
     <section
-      className="w-full h-[70vh] bg-cover bg-center"
+      className='w-full h-[70vh] bg-cover bg-center flex items-center justify-center'
       style={{ backgroundImage: `url(${banner})` }}
     >
-      {/* Content here */}
+      <div className='w-[70%] h-[75%] bg-gray-100/20'>
+        <h1 className='text-white font-sora text-3xl text-center'>Welcome to Luminara</h1>
+      </div>
     </section>
   );
 };
