96493ad4a749f431256b8ff942d81e0711e6393e
Daniel <danielkibret8@gmail.com>
2025-05-24 17:29:32 +0300

    Navigations

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,55 +1,11 @@
-import { useState } from "react";
-import { navigations } from "../constants";
-import Button from "./Button";
-import logo from '../assets/logo.png';
-const Navbar = () => {
-  const [toggleNavigation, setToggleNavigation] = useState(false);
-
-  const handleToggleNavigation = () => {
-    setToggleNavigation((prev) => !prev);
-  };
-
+const Navbar = () =>{
   return (
-    <div className="w-full fixed bg-white z-50">
-      <div className="w-full flex justify-between items-center px-10">
-        <a className="w-[10rem]  p-2" href="/">
-          <img src={logo} alt='LUMINARA'/>
-        </a>
-        <button
-          className="lg:hidden cursor-pointer text-white"
-          onClick={handleToggleNavigation}
-        >
-          {toggleNavigation ? <span>X</span> : <span>Menu</span>}
-        </button>
+    <section className='sticky top-0 left-0 w-full h-[10vh] flex items-center justify-center'>
+      <div className='w-[90%] bg-red-600 h-full'>
 
-        <nav
-          className={`${
-            toggleNavigation ? "flex" : "hidden"
-          } fixed top-[2rem] left-0 right-0 bottom-0 lg:static lg:flex lg:mx-auto lg:bg-transparent`}
-        >
-          <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
-            {navigations.map((item) => (
-              <a className="font-lightbold font-Sora mx-[1em] my-[1em] tracking-wide
-                            "key={item.id} href={item.url}>
-                {item.title}
-              </a>
-            ))}
-          </div>
-        </nav>
-        <div className='flex justify-around'>
-        <Button
-           className={'hidden lg:flex w-[5em] h-[2.1em] mx-[1em] border border-[#40E0D0]'}
-           title={'Login'}
-           span={'text-[#40E0D0]'}
-        />
-                <Button
-           className={'hidden lg:flex w-[9em] h-[2.1em] bg-[#40E0D0]'}
-           title={'Create Account'}
-        />
-        </div>
       </div>
-    </div>
+    </section>
   );
-};
+}
 
 export default Navbar;
