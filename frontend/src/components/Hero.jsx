843b9d166e064bd8daca7f57c7857e1ba032a1f5
Daniel <danielkibret8@gmail.com>
2025-05-24 22:54:20 +0300

    UI

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -1,4 +1,5 @@
 import banner from '../assets/banner.jpg';
+import Button from './Button';
 
 const Hero = () => {
   return (
@@ -6,8 +7,20 @@ const Hero = () => {
       className='w-full h-[70vh] bg-cover bg-center flex items-center justify-center'
       style={{ backgroundImage: `url(${banner})` }}
     >
-      <div className='w-[70%] h-[75%] bg-gray-100/20'>
-        <h1 className='text-white font-sora text-3xl text-center'>Welcome to Luminara</h1>
+      <div className='w-[70%] h-[75%] bg-black/20 flex flex-col items-center justify-around'>
+        <h1 className='text-golden font-sora text-3xl text-center font-bold'>Welcome to Luminara</h1>
+        <p className='text-center text-white w-[80%] font-sora my-4'>
+          Experience seamless hotel management with Luminara — your all-in-one platform for effortless reservations,
+          guest services, and luxury experiences. Designed for modern hotels that value excellence, efficiency, and
+          unforgettable stays.Let your guests shine. Let Luminara handle the rest.
+        </p>
+        <Button
+          className={'bg-primary font-sora p-2 '}
+          title={'Explore More'}
+
+        >
+
+        </Button>
       </div>
     </section>
   );
