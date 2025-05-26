d1b0b695e9065a160492f1066eedff27dee80f9f
Daniel <danielkibret8@gmail.com>
2025-05-26 17:48:54 +0300

    Login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -3,14 +3,14 @@ import Button from '../components/Button';
 import { useState } from 'react';
 const Login = () => {
 
-  const inputStyle = `w-full bg-none  outline-0 border-b border-golden`;
+  const inputStyle = `w-full bg-none outline-0 border-1 border-primary rounded-md p-2`;
   return (
     <section className='w-full h-[87vh] flex items-center justify-center bg-center bg-cover'
              style={{backgroundImage :`url(${banner})`}}
     >
       <div className='w-[20%] h-[70%] bg-white/10 backdrop-blur-md rounded-md shadow-md border border-white/20 p-4'>
-        <div className='w-full h-[95%]'>
-          <h1 className='text-3xl font-bold text-golden'>Login</h1>
+        <div className='relative w-full h-full '>
+          <h1 className='text-3xl font-bold text-primary'>Login</h1>
           <div className='w-[90%] h-[90%] flex flex-col items-center justify-around '>
             <input type='text'
                    name='_userName'
@@ -25,6 +25,12 @@ const Login = () => {
               title={'Submit'}
             ></Button>
           </div>
+          <a 
+             href="/register" 
+             className="absolute bottom-0 left-1/2 transform -translate-x-1/2 text-blue-700"
+          >
+            Create Account
+          </a>
         </div>
       </div>
     </section>
