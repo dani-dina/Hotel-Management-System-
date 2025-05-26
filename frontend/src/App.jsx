b3d905d9903603104da9390845973caa6770b771
Daniel <danielkibret8@gmail.com>
2025-05-26 23:24:42 +0300

    Rooms

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import Navbar from './components/Navbar';
 import Info from './components/Info';
 import Footer from './components/Footer';
 import Register from './pages/Register';
+import Rooms from './pages/Rooms';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
         <Route path='/' element={<Home />} />
         <Route path='/sign-up' element={<Login />} />
         <Route path='/sign-in' element={<Register />} />
+        <Route path='/rooms' element={<Rooms />} />
         <Route path='*' element={<NotFound />}/>
       </Routes>
     <Footer />
