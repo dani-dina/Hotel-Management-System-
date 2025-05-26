f9cf3d39d5a742a62dee110ea032df38aed89658
Daniel <danielkibret8@gmail.com>
2025-05-26 23:54:41 +0300

    contact

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -7,6 +7,7 @@ import Info from './components/Info';
 import Footer from './components/Footer';
 import Register from './pages/Register';
 import Rooms from './pages/Rooms';
+import ContactUs from './pages/Contact';
 
 function App() {
   return (
@@ -19,6 +20,7 @@ function App() {
         <Route path='/sign-in' element={<Register />} />
         <Route path='/rooms' element={<Rooms />} />
         <Route path='*' element={<NotFound />}/>
+        <Route path='*' element={<ContactUs />}/>
       </Routes>
     <Footer />
     </div>
