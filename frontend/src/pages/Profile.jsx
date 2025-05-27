d8121f2439eebbc43f3c29c99d5271ec3fab6166
Daniel <danielkibret8@gmail.com>
2025-05-27 22:57:20 +0300

    profile

--- frontend/src/pages/Profile.jsx
+++ frontend/src/pages/Profile.jsx
@@ -54,15 +54,7 @@ const ProfilePage = () => {
           <p className="mt-3 text-gray-700 dark:text-gray-300">{user.bio}</p>
           
           <div className="flex flex-wrap gap-x-4 mt-3 text-gray-600 dark:text-gray-400">
-            {user.location && (
-              <div className="flex items-center">
-                <svg className="w-5 h-5 mr-1" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z" />
-                  <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M15 11a3 3 0 11-6 0 3 3 0 016 0z" />
-                </svg>
-                <span>{user.location}</span>
-              </div>
-            )}
+
             
 
             
