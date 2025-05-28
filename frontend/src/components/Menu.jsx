ef9608b0f1f1f334b364ae4364b95f369bddc770
Daniel <danielkibret8@gmail.com>
2025-05-28 23:16:40 +0300

    Menu

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -81,7 +81,7 @@ const MenuPage = () => {
         </div>
       </header>
       <main className="max-w-7xl mx-auto px-4 py-6 sm:px-6 lg:px-8">
-        {/* Category Filter */}
+       
         <div className="mb-8 overflow-x-auto">
           <div className="flex space-x-4 pb-2">
             {categories.map(category => (
