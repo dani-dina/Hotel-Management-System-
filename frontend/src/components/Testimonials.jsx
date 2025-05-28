7bae325e79c12dd11907d70d433f0ba35f4953f5
Daniel <danielkibret8@gmail.com>
2025-05-28 23:25:17 +0300

    Testimonials

--- frontend/src/components/Testimonials.jsx
+++ frontend/src/components/Testimonials.jsx
@@ -146,7 +146,7 @@ const TestimonialPage = () => {
           ))}
         </div>
 
-        {/* Empty State */}
+      
         {filteredTestimonials.length === 0 && (
           <div className="text-center py-12">
             <svg
