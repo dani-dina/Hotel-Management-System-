3de9a1fac6774e5b4567c9bca6bec1865710c809
Daniel <danielkibret8@gmail.com>
2025-05-29 23:43:14 +0300

    store

---
+++ frontend/src/store/orderSlice.js
@@ -0,0 +1,36 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { createOrder } from './orderThunks';
+
+const initialState = {
+  currentOrder: null,
+  loading: false,
+  error: null
+};
+
+const orderSlice = createSlice({
+  name: 'orders',
+  initialState,
+  reducers: {
+    clearCurrentOrder: (state) => {
+      state.currentOrder = null;
+    }
+  },
+  extraReducers: (builder) => {
+    builder
+      .addCase(createOrder.pending, (state) => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(createOrder.fulfilled, (state, action) => {
+        state.loading = false;
+        state.currentOrder = action.payload;
+      })
+      .addCase(createOrder.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      });
+  }
+});
+
+export const { clearCurrentOrder } = orderSlice.actions;
+export default orderSlice.reducer;
