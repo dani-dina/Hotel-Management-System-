7c8d367759abdd8b238a6ebb436d9d68916f687a
Daniel <danielkibret8@gmail.com>
2025-05-29 23:36:09 +0300

    store

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -1,5 +1,5 @@
 import { configureStore } from '@reduxjs/toolkit';
-import cartReducer from '../features/cart/cartSlice';
+import cartReducer from '../store/cartSlice.js';
 
 const store = configureStore({
   reducer: {
