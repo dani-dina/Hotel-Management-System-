b3467635dbdc39429b56eccd0972b555af7e8074
Daniel <danielkibret8@gmail.com>
2025-05-29 23:53:08 +0300

    payment

---
+++ backend/src/config/paypal.js
@@ -0,0 +1,17 @@
+// config/paypal.js
+const paypal = require('@paypal/checkout-server-sdk');
+
+// Configure PayPal SDK
+const configureEnvironment = function() {
+  const clientId = process.env.PAYPAL_CLIENT_ID;
+  const clientSecret = process.env.PAYPAL_CLIENT_SECRET;
+
+  if (process.env.NODE_ENV === 'production') {
+    return new paypal.core.LiveEnvironment(clientId, clientSecret);
+  }
+  return new paypal.core.SandboxEnvironment(clientId, clientSecret);
+};
+
+const client = new paypal.core.PayPalHttpClient(configureEnvironment());
+
+module.exports = client;
