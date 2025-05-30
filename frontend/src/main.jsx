c44659751f7cf07dfda317d384b104445f617787
Daniel <danielkibret8@gmail.com>
2025-05-31 01:56:54 +0300

    main

---
+++ frontend/src/main.jsx
@@ -0,0 +1,18 @@
+import './index.css';
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import { Provider } from 'react-redux';
+import { BrowserRouter } from 'react-router-dom'; // ← Add this
+import store from './store/store'; // Adjust path as needed
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <BrowserRouter> {/* ← Wrap App for routing */}
+        <App />
+      </BrowserRouter>
+    </Provider>
+  </React.StrictMode>
+);
