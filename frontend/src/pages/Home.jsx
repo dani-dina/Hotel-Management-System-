d0a4e16fde79bf924a3e342557c0f1db9817a622
Daniel <danielkibret8@gmail.com>
2025-05-31 23:53:28 +0300

    ui

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,14 +3,29 @@ import Hero from '../components/Hero';
 import waiter from '../assets/waiter.jpg';
 import Rooms from '../components/Rooms';
 import SupportIcon from '../components/Support';
+import Button from '../components/Button';
 const Home = () => {
   return (
     <div className='w-full overflow-hidden'>
       <Hero />
       <SupportIcon/>
       {/* About Section */}
-      <section className='w-full py-12 md:py-24 px-4 sm:px-6 lg:px-8'>
-        
+      <section className='w-full py-12 md:py-24 px-4 sm:px-6 lg:px-8 flex items-center justify-center'>
+        <div className='w-[90%] bg-red-400'>
+          <div className=''>
+            <h1>Best offer this month </h1>
+            <div>
+              <p>This can be detailed description or just a short text </p>
+              <Button
+                className={'bg-'}
+                title={'view all'}
+              />
+            </div>
+          </div>
+          <div>
+
+          </div>
+        </div>
       </section>
 
       {/* Divider */}
