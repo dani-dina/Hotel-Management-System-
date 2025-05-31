df45bc28ce34b99b509ca55e69dc9294a1358fb3
Daniel <danielkibret8@gmail.com>
2025-05-31 23:54:43 +0300

    ui

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -14,7 +14,7 @@ const Home = () => {
         <div className='w-[90%] bg-red-400'>
           <div className=''>
             <h1>Best offer this month </h1>
-            <div className='w-full flex'>
+            <div className='w-full flex items-center justify-between'>
               <p>This can be detailed description or just a short text </p>
               <Button
                 className={'bg-gray-200 text-primary '}
