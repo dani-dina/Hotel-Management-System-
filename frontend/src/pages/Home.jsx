ec5c885bfce561b49ee651db75fc0e0383480804
Daniel <danielkibret8@gmail.com>
2025-05-31 23:56:30 +0300

    ui

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -13,14 +13,13 @@ const Home = () => {
       <section className='w-full py-12 md:py-24 px-4 sm:px-6 lg:px-8 flex items-center justify-center'>
         <div className='w-[90%]'>
           <div className=''>
-            <h1 className='text-3xl font-semibold font-poppins'>Best offer this month </h1>
+            <h1 className='text-4xl font-semibold font-poppins'>Best offer this month </h1>
             <div className='w-full flex items-center justify-between'>
               <p>This can be detailed description or just a short text </p>
               <Button
                 className={'bg-gray-200'}
                 title={'view all'}
                 span={'text-primary'}
-
               />
             </div>
           </div>
