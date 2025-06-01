d8e85047eaeb7677c822bf74fad642ff03c000bd
Daniel <danielkibret8@gmail.com>
2025-06-01 23:44:13 +0300

    ui

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -10,7 +10,7 @@ const Home = () => {
     <div className='w-full overflow-hidden'>
       <Hero />
       <SupportIcon/>
-      {/* About Section */}
+      {/* About */}
       <section className='w-full py-8 px-4 sm:px-6 lg:px-8 flex items-center justify-center'>
         <div className='w-[90%]'>
           <div className='my-4'>
