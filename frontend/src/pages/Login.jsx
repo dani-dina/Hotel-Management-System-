f976f792d06af8e7a593b4c97ed689ec9b6b7da8
Daniel <danielkibret8@gmail.com>
2025-06-10 22:46:58 +0300

    Change

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -38,8 +38,6 @@ const Login = () => {
             Log in
           </button>
         </form>
-        
-        {/* Divider */}
         <div className="relative my-6">
           <div className="absolute inset-0 flex items-center">
             <div className="w-full border-t border-gray-300"></div>
