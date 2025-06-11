136244144876f7f4b4e0014c161a6da890fcc51e
Daniel <danielkibret8@gmail.com>
2025-06-11 18:46:42 +0300

    Frontend

--- frontend/src/components/Jumbotron.jsx
+++ frontend/src/components/Jumbotron.jsx
@@ -19,8 +19,22 @@ const Jumbotron = () => {
         }
       </div>
       <div className='w-full h-3/4 flex items-center justify-around'>
-        <div className='h-full w-1/5 bg-gray-500'>
-          
+        <div className='h-full w-1/5 '>
+          <div className="w-full max-w-sm mx-auto mt-8">
+            <select
+              id="country"
+              value={selected}
+              onChange={(e) => setSelected(e.target.value)}
+              className="block w-3/4 px-4 py-2 border border-gray-300 rounded-md focus:outline-none bg-white text-gray-800"
+            >
+            <option value="" disabled>Select a country</option>
+            {countries.map((country) => (
+              <option key={country} value={country}>
+                {country}
+              </option>
+            ))}
+          </select>
+    </div>
         </div>
         <div className='h-full w-1/5 bg-gray-500'>
 
