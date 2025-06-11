6a3275d9919d78c1b088951e7672470cece7afbd
Daniel <danielkibret8@gmail.com>
2025-06-11 18:13:12 +0300

    Frontend

--- frontend/src/components/Jumbotron.jsx
+++ frontend/src/components/Jumbotron.jsx
@@ -1,8 +1,14 @@
+import { jumbotronLinks } from '../constants';
+
 const Jumbotron = () => {
   return (
     <section className='w-[95%] h-[90%] bg-red-800 flex flex-col items-center justify-between'>
-      <div className='w-full h-1/3 bg-green-400'>
-
+      <div className='w-full h-1/3'>
+        {
+          jumbotronLinks.map((items) => (
+            <a key={items.id}>{items.title}</a>
+          ))
+        }
       </div>
       <div className='w-full h-1/3 bg-blue-50'>
 
