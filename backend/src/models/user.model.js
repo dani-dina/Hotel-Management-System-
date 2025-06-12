2578ff69c310be5ef7a72a5f6b4ae3399a7395f8
Daniel <danielkibret8@gmail.com>
2025-06-12 23:47:50 +0300

    UI

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -5,7 +5,7 @@ const userAccountSchema = new mongoose.Schema({
     accountId: { type: String, required: true, unique: true },  
     email: { type: String, required: true, unique: true },  
     phoneNumber: { type: String, required: true, unique: true },  
-    password: { type: String, required: true }, // Hashed password  
+    password: { type: String, required: true },
     role: { 
         type: String, 
         required: true, 
