03a6ef5fc7f0ad00afb6fd6d527dda3cc66b384e
Daniel <danielkibret8@gmail.com>
2025-06-12 23:47:19 +0300

    UI

--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -23,7 +23,7 @@ const userAccountSchema = new mongoose.Schema({
 
 // Hash password before saving a new user or updating the password
 userAccountSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) return next(); // Only hash if password is modified
+    if (!this.isModified('password')) return next(); 
     try {
         const salt = await bcrypt.genSalt(10);
         this.password = await bcrypt.hash(this.password, salt);
