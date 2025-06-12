283313a0c97ef5ec392356bf3806d806bd1c5f53
Daniel <danielkibret8@gmail.com>
2025-06-12 23:27:38 +0300

    UI

--- frontend/src/pages/Order.jsx
+++ frontend/src/pages/Order.jsx
@@ -369,8 +369,6 @@ const OrderPage = () => {
           </div>
         </div>
       )}
-
-      {/* Create Order  */}
       {isModalOpen && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center p-4 z-50">
           <div className="bg-white rounded-lg shadow-xl max-w-md w-full">
