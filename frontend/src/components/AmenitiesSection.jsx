83c531f2b000b53a2e2b0806829dfd6dacd8f6af
Daniel <danielkibret8@gmail.com>
2025-06-18 23:55:04 +0300

    major change

--- frontend/src/components/AmenitiesSection.jsx
+++ frontend/src/components/AmenitiesSection.jsx
@@ -124,7 +124,6 @@ const AmenitiesSection = () => {
         </div>
       </div>
 
-      {/* Packages Modal */}
       {showPackages && (
         <div className="fixed inset-0 bg-black/50 z-50 flex items-center justify-center p-4">
           <div className="bg-white rounded-xl max-w-4xl w-full max-h-[90vh] overflow-y-auto">
