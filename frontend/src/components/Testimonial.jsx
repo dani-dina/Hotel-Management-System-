cf500b28521b950d05339753335bbca7a1caf78e
Daniel <danielkibret8@gmail.com>
2025-07-09 23:57:22 +0300

    changes

--- frontend/src/components/Testimonial.jsx
+++ frontend/src/components/Testimonial.jsx
@@ -76,7 +76,7 @@ const Testimonials = () => {
         </p>
         
         <div className="relative max-w-4xl mx-auto">
-          {/* Testimonials Carousel */}
+          {/* Testimonials  */}
           <div className="overflow-hidden">
             <div 
               className="flex transition-transform duration-300 ease-in-out"
